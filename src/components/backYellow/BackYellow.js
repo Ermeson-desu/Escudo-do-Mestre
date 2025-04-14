e1accdc0a81b288c38bc2915df93c7a521069dfe
Ermeson-desu <ermesondesu@gmail.com>
2025-04-14 17:51:10 -0300

    feat-ajeitando styles da tela escudo do mestre

--- src/components/backYellow/BackYellow.js
+++ src/components/backYellow/BackYellow.js
@@ -1,8 +1,14 @@
-import {StylesBackYellow, BaseView} from './Styles';
+import {StylesBackYellow, BaseView, StylesBlockUp} from './Styles';
+import { ButtonText } from '../button/Style';
 
 export const BackYellow = () => {
     return(
         <BaseView>
+            <StylesBlockUp>
+                <ButtonText>
+                    Nota
+                </ButtonText>
+            </StylesBlockUp>
             <StylesBackYellow>
 
             </StylesBackYellow>  
--- src/components/backYellow/Styles.js
+++ src/components/backYellow/Styles.js
@@ -2,13 +2,25 @@ import styled from "styled-components";
 
 export const BaseView = styled.View`
     flex:1;
+    margin-left: 10%;
     width: 100%;
-    justify-content:center;
-    align-items: ;
+    justify-content:flex-end;
+    align-items: flex-start;
 `;
 
 export const StylesBackYellow = styled.View`
     background-color: #FEDAA5;
     width: 90%;
-    height:80%;
+    height:92%;
+    border-top-right-radius:10px;
 `;
+
+export const StylesBlockUp = styled.View`
+    background-color: #FEDAA5;
+    width: 15%;
+    height:5%;
+    border-top-left-radius:10px;
+    border-top-right-radius:10px;
+    justify-content:center;
+`;
+
